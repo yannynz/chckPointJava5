9ff3fe73a0080b211d7bea2f4cc9a9b5073f6ce5
Yann Santana <testeyann@hotmail.com>
2022-10-11 14:38:04 -0300

    d tarde

--- chckPointJava/ex3/ex3.java
+++ chckPointJava/ex3/ex3.java
@@ -10,25 +10,20 @@ public class ex3 {
         System.out.print("Informe a quantidade de triângulos: ");
         qtdTri = key.nextInt();
 
-        int ladoTri[][] = new int[2][qtdTri];
+        int ladoTri[][] = new int[3][qtdTri];
 
-        for (int i = 0; i < qtdTri; i++) {
-            for (int j = 0; j < ladoTri.length; j++) {
-                System.out.print("Informe o valor de cada lado do triangulo" + (i + 1) + ": ");
+        for (int j = 0; j < qtdTri; j++) {
+            for (int i = 0; i < ladoTri.length; i++) {
+                System.out.print("Informe o valor de cada lado do triangulo " + (j + 1) + ": ");
                 ladoTri[i][j] = key.nextInt();
-            }
-        }
-        for (int i = 0; i < ladoTri.length; i++) {
-            for (int j = 0; j < ladoTri.length; j++) {
-            
-                if (ladoTri[i][j] + ladoTri[i][j + 1] < ladoTri[i][j]) {
+                if (ladoTri[i][j] + ladoTri[i][j+1] > ladoTri[i][j]) {
                     System.out.print("Valores invalidos");
                     break;
                 }
             }
-                
-            }
-        for (int i = 0; i < qtdTri; i++) {
+        }
+        
+      for (int i = 0; i < qtdTri; i++) {
             for (int j = 0; j < ladoTri.length; j++) {
                 if (ladoTri[i][j] == ladoTri[i][j + 1] && ladoTri[i][j] == ladoTri[i][j + 1]) {
                     System.out.println("O triangulo " + (i + 1) + " é equilátero");
