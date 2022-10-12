fd90770b844448962e97f61f60260485d5215711
Yann Santana <testeyann@hotmail.com>
2022-10-12 12:53:01 -0300

    ex2 cabo

--- chckPointJava/ex2/ex2.java
+++ chckPointJava/ex2/ex2.java
@@ -7,19 +7,14 @@ public class ex2 {
 
         Scanner key = new Scanner(System.in);
         int numUsr = 0 ;
-        System.out.print("Informe um número interio e positivo: ");
+        System.out.print("Informe um número interio e positivo para transforma-lo em binário: ");
         numUsr = key.nextInt();
-        for (int i = 0; i < 1; i++) {
             if (numUsr < 0) {
                 System.out.print("Número invalido, favor reiniciar o programa e inserir um número válido ");
-                break; 
-            }
-        }
-        
-        while (numUsr>0) {
+            }else{ while (numUsr>0) {
             numUsr=numUsr/2;
-            System.out.print("O número informado, em binário fica: "+numUsr%2);
-        }
+            System.out.print(numUsr%2);
+        }}
               
         
 
