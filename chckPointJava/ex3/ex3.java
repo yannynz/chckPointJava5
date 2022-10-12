d28a093ef990405de9b2b09845f12d024b09624c
Yann Santana <testeyann@hotmail.com>
2022-10-12 13:54:55 -0300

    cabo

--- chckPointJava/ex3/ex3.java
+++ chckPointJava/ex3/ex3.java
@@ -6,31 +6,31 @@ public class ex3 {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
         int qtdTri;
-
+        int ladoTri, ladoTri1, ladoTri2;
         System.out.print("Informe a quantidade de triângulos: ");
         qtdTri = key.nextInt();
-
-        int ladoTri[][] = new int[qtdTri][3];
-
         for (int i = 0; i < qtdTri; i++) {
-            for (int j = 0; j < ladoTri.length; j++) {
-                System.out.print("Informe o valor de cada lado do triangulo " + (j + 1) + ": ");
-                ladoTri[i][j] = key.nextInt();
-                if (ladoTri[i][j] < ladoTri[i][j+1] + ladoTri[i][j+2] && ladoTri[i][j+1] < ladoTri[i][j] + ladoTri[i][j+2] && ladoTri[i][j+2] < ladoTri[i][j] + ladoTri[i][j+1]) {
-                    if (ladoTri[i][j] == ladoTri[i][j + 1] && ladoTri[i][j+1] == ladoTri[i][j + 2]) {
-                        System.out.println("O triangulo " + (i + 1) + " é equilátero");
-                    }if (ladoTri[i][j] == ladoTri[i][j+1] || ladoTri[i][j] == ladoTri[i][j+2] || ladoTri[i][j+1] == ladoTri[i][j+2]) {
-                        System.out.println("O triangulo " + (i + 1) + "  é isósceles");
-                    }else{ System.out.println("O triangulo " + (i + 1) + " é escaleno");}
-                    
+            System.out.println("Triângulo " + (i + 1));
+            System.out.print("Informe o valor do lado 1: ");
+            ladoTri = key.nextInt();
+            System.out.print("Informe o valor do lado 2:");
+            ladoTri1 = key.nextInt();
+            System.out.print("Informe o valor do lado 3: ");
+            ladoTri2 = key.nextInt();
+            if (ladoTri < ladoTri1 + ladoTri && ladoTri1 < ladoTri + ladoTri2 && ladoTri2 < ladoTri1 + ladoTri) {
+                if (ladoTri == ladoTri1 && ladoTri1 == ladoTri2) {
+                    System.out.println("Triângulo equilátero \n");
+                }else if (ladoTri == ladoTri1 || ladoTri1 == ladoTri2 || ladoTri2 == ladoTri1) {
+                    System.out.println("Triângulo isóceles \n");
+                }else {
+                    System.out.println("Triângulo escaleno \n");
                 }
+                } else {
+                System.out.println(
+                        "Valores informados são invalidos, reinicie a aplicação e insira os valores corretamente");
+                        break;
             }
         }
-        
-    //   for (int i = 0; i < qtdTri; i++) {
-    //         for (int j = 0; j < ladoTri.length; j++) {
-    //             }
-    //         }
-        }
-    }
 
+    }
+}
