9ff751d98eb206ae7de7086cb00c85b0139b68e1
Yann Santana <testeyann@hotmail.com>
2022-10-12 17:01:13 -0300

    final5.0

--- chckPointJava/ex1/ex1.java
+++ chckPointJava/ex1/ex1.java
@@ -7,20 +7,17 @@ public class ex1 {
     public static void main(String[] args) {
 
         Scanner key = new Scanner(System.in);
-// variaveis
         int mkt, product;
         double finalPrice, priceQtd;
         System.out.print("Informe a quantidade de supermercados a serem pesquisados: ");
         mkt = key.nextInt();
         System.out.print("Informe a quantidade de produtos a serem pesquisados nos supermercados: ");
         product = key.nextInt();
-// Arrays
         int qtdProduct[] = new int[product];
         double mktTable[][] = new double[product][mkt];
-// contas
         for (int i = 0; i < mktTable.length; i++) {
             System.out.println("Informe a quantidade do produto " + (i + 1)
-                    + "( a quantidade deve ser infomada sem qualquer unidade de medida antes ou após o número)");
+            + "( a quantidade deve ser infomada sem qualquer unidade de medida antes ou após o número)");
             qtdProduct[i] = key.nextInt();
         }for (int i = 0; i < mktTable.length; i++) {
             System.out.print("Supermercado " + (i + 1));
@@ -32,7 +29,7 @@ public class ex1 {
                 System.out.println("O preço total do produto " + (j + 1) + " em reais R$ é de:" + priceQtd);
                 finalPrice += priceQtd;
                 priceQtd = 0;
-            }//final
+            }
             System.out.println("O total a ser pago no Supermercado " + (i + 1) + " é de: R$ " + finalPrice);
         }
 
